bb9b48d3c227d1b6aa0629adecb78c067ee94112
Jared Gould <jared.gould@gmail.com>
2006-07-01 16:31:36 +0000

    Updated version number and release date
    git-svn-id: http://svn.3splooges.com/romraider-arch/trunk@54 d2e2e1cd-ba16-0410-be16-b7c4453c7c2d

--- src/enginuity/ECUEditor.java
+++ src/enginuity/ECUEditor.java
@@ -44,8 +44,8 @@ public class ECUEditor extends JFrame implements WindowListener {
     private RomTree      imageList           = new RomTree(imageRoot);
     private Vector<Rom>  images              = new Vector<Rom>();
     private Settings     settings            = new Settings();
-    private String       version             = new String("0.2.90 Pre-Release Alpha");
-    private String       versionDate         = new String("6/30/2006");
+    private String       version             = new String("0.2.92 Pre-Release Alpha");
+    private String       versionDate         = new String("7/1/2006");
     private String       titleText           = new String("Enginuity v" + version);
     private MDIDesktopPane rightPanel        = new MDIDesktopPane();    
     private Rom          lastSelectedRom     = null;
