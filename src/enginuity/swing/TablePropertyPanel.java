5e8082b86c184a6e5ec0dfdbab757d9a1567c0c8
Jared Gould <jared.gould@gmail.com>
2006-07-03 14:58:10 +0000

    Updated table property panel and fixed scrolling issues
    git-svn-id: http://svn.3splooges.com/romraider-arch/trunk@77 d2e2e1cd-ba16-0410-be16-b7c4453c7c2d

--- src/enginuity/swing/TablePropertyPanel.java
+++ src/enginuity/swing/TablePropertyPanel.java
@@ -29,23 +29,27 @@ public class TablePropertyPanel extends javax.swing.JPanel {
         tableName = new javax.swing.JLabel();
         lblCategory = new javax.swing.JLabel();
         category = new javax.swing.JLabel();
-        lblStorageSize = new javax.swing.JLabel();
-        lblStorageAddress = new javax.swing.JLabel();
-        lblEndian = new javax.swing.JLabel();
-        lblConversion = new javax.swing.JLabel();
-        lblStorage = new javax.swing.JLabel();
-        lblByteToReal = new javax.swing.JLabel();
-        lblRealToByte = new javax.swing.JLabel();
+        jPanel1 = new javax.swing.JPanel();
         lblUnit = new javax.swing.JLabel();
+        unit = new javax.swing.JLabel();
+        lblByteToReal = new javax.swing.JLabel();
         byteToReal = new javax.swing.JLabel();
         realToByte = new javax.swing.JLabel();
-        unit = new javax.swing.JLabel();
-        storageAddress = new javax.swing.JLabel();
-        storageSize = new javax.swing.JLabel();
+        lblRealToByte = new javax.swing.JLabel();
+        jPanel2 = new javax.swing.JPanel();
+        lblStorageAddress = new javax.swing.JLabel();
+        lblStorageSize = new javax.swing.JLabel();
+        lblEndian = new javax.swing.JLabel();
         endian = new javax.swing.JLabel();
-        lblDescription = new javax.swing.JLabel();
-        description = new javax.swing.JLabel();
+        storageSize = new javax.swing.JLabel();
+        storageAddress = new javax.swing.JLabel();
+        jPanel3 = new javax.swing.JPanel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        description = new javax.swing.JTextArea();
 
+        setAutoscrolls(true);
+        setFont(new java.awt.Font("Tahoma", 0, 12));
+        setInheritsPopupMenu(true);
         lblTable.setText("Table:");
         lblTable.setFocusable(false);
 
@@ -58,29 +62,15 @@ public class TablePropertyPanel extends javax.swing.JPanel {
         category.setText("Category");
         category.setFocusable(false);
 
-        lblStorageSize.setText("Storage Size:");
-        lblStorageSize.setFocusable(false);
+        jPanel1.setBorder(javax.swing.BorderFactory.createTitledBorder(javax.swing.BorderFactory.createTitledBorder("Conversion")));
+        lblUnit.setText("Unit:");
+        lblUnit.setFocusable(false);
 
-        lblStorageAddress.setText("Storage Address:");
-        lblStorageAddress.setFocusable(false);
-
-        lblEndian.setText("Endian:");
-        lblEndian.setFocusable(false);
-
-        lblConversion.setText("----------- Conversion -----------");
-        lblConversion.setFocusable(false);
-
-        lblStorage.setText("---------- Storage ----------");
-        lblStorage.setFocusable(false);
+        unit.setText("unit");
+        unit.setFocusable(false);
 
         lblByteToReal.setText("Byte to Real:");
         lblByteToReal.setFocusable(false);
-
-        lblRealToByte.setText("Real to Byte:");
-        lblRealToByte.setFocusable(false);
-
-        lblUnit.setText("Unit:");
-        lblUnit.setFocusable(false);
 
         byteToReal.setText("bytetoreal");
         byteToReal.setFocusable(false);
@@ -88,121 +78,175 @@ public class TablePropertyPanel extends javax.swing.JPanel {
         realToByte.setText("realtobyte");
         realToByte.setFocusable(false);
 
-        unit.setText("unit");
-        unit.setFocusable(false);
+        lblRealToByte.setText("Real to Byte:");
+        lblRealToByte.setFocusable(false);
 
-        storageAddress.setText("0x00");
-        storageAddress.setFocusable(false);
+        org.jdesktop.layout.GroupLayout jPanel1Layout = new org.jdesktop.layout.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+            .add(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .add(jPanel1Layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+                    .add(jPanel1Layout.createSequentialGroup()
+                        .add(lblUnit)
+                        .add(48, 48, 48)
+                        .add(unit))
+                    .add(jPanel1Layout.createSequentialGroup()
+                        .add(jPanel1Layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+                            .add(lblByteToReal)
+                            .add(lblRealToByte))
+                        .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
+                        .add(jPanel1Layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+                            .add(byteToReal)
+                            .add(realToByte))))
+                .addContainerGap(43, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+            .add(jPanel1Layout.createSequentialGroup()
+                .add(jPanel1Layout.createParallelGroup(org.jdesktop.layout.GroupLayout.BASELINE)
+                    .add(lblUnit)
+                    .add(unit))
+                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
+                .add(jPanel1Layout.createParallelGroup(org.jdesktop.layout.GroupLayout.TRAILING)
+                    .add(jPanel1Layout.createSequentialGroup()
+                        .add(lblByteToReal)
+                        .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
+                        .add(lblRealToByte))
+                    .add(jPanel1Layout.createSequentialGroup()
+                        .add(byteToReal)
+                        .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
+                        .add(realToByte)))
+                .addContainerGap(org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
 
-        storageSize.setText("uint16");
-        storageSize.setFocusable(false);
+        jPanel2.setBorder(javax.swing.BorderFactory.createTitledBorder("Storage"));
+        lblStorageAddress.setText("Storage Address:");
+        lblStorageAddress.setFocusable(false);
+
+        lblStorageSize.setText("Storage Size:");
+        lblStorageSize.setFocusable(false);
+
+        lblEndian.setText("Endian:");
+        lblEndian.setFocusable(false);
 
         endian.setText("little");
         endian.setFocusable(false);
 
-        lblDescription.setText("Description:");
-        lblDescription.setFocusable(false);
+        storageSize.setText("uint16");
+        storageSize.setFocusable(false);
 
-        description.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
+        storageAddress.setText("0x00");
+        storageAddress.setFocusable(false);
+
+        org.jdesktop.layout.GroupLayout jPanel2Layout = new org.jdesktop.layout.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+            .add(jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
+                .add(jPanel2Layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+                    .add(lblStorageAddress)
+                    .add(lblStorageSize)
+                    .add(lblEndian))
+                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
+                .add(jPanel2Layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+                    .add(endian)
+                    .add(storageSize)
+                    .add(storageAddress))
+                .addContainerGap(52, Short.MAX_VALUE))
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+            .add(jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
+                .add(jPanel2Layout.createParallelGroup(org.jdesktop.layout.GroupLayout.BASELINE)
+                    .add(lblStorageSize)
+                    .add(storageSize))
+                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
+                .add(jPanel2Layout.createParallelGroup(org.jdesktop.layout.GroupLayout.BASELINE)
+                    .add(lblStorageAddress)
+                    .add(storageAddress))
+                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
+                .add(jPanel2Layout.createParallelGroup(org.jdesktop.layout.GroupLayout.BASELINE)
+                    .add(lblEndian)
+                    .add(endian))
+                .addContainerGap(8, Short.MAX_VALUE))
+        );
+
+        jPanel3.setBorder(javax.swing.BorderFactory.createTitledBorder("Description"));
+        jScrollPane1.setBorder(null);
+        description.setBackground(new java.awt.Color(236, 233, 216));
+        description.setColumns(20);
+        description.setEditable(false);
+        description.setFont(new java.awt.Font("Tahoma", 0, 11));
+        description.setLineWrap(true);
+        description.setRows(5);
         description.setText("Description");
-        description.setVerticalAlignment(javax.swing.SwingConstants.TOP);
-        description.setFocusable(false);
+        description.setWrapStyleWord(true);
+        description.setBorder(null);
+        description.setOpaque(false);
+        description.setRequestFocusEnabled(false);
+        jScrollPane1.setViewportView(description);
+
+        org.jdesktop.layout.GroupLayout jPanel3Layout = new org.jdesktop.layout.GroupLayout(jPanel3);
+        jPanel3.setLayout(jPanel3Layout);
+        jPanel3Layout.setHorizontalGroup(
+            jPanel3Layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+            .add(jPanel3Layout.createSequentialGroup()
+                .addContainerGap()
+                .add(jScrollPane1, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 363, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+        jPanel3Layout.setVerticalGroup(
+            jPanel3Layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+            .add(jPanel3Layout.createSequentialGroup()
+                .add(jScrollPane1, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 102, Short.MAX_VALUE)
+                .addContainerGap())
+        );
 
         org.jdesktop.layout.GroupLayout layout = new org.jdesktop.layout.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-            .add(layout.createSequentialGroup()
+            .add(org.jdesktop.layout.GroupLayout.TRAILING, layout.createSequentialGroup()
                 .addContainerGap()
-                .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-                    .add(description, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 399, Short.MAX_VALUE)
-                    .add(lblDescription)
-                    .add(org.jdesktop.layout.GroupLayout.TRAILING, layout.createSequentialGroup()
+                .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.TRAILING)
+                    .add(org.jdesktop.layout.GroupLayout.LEADING, jPanel3, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .add(org.jdesktop.layout.GroupLayout.LEADING, layout.createSequentialGroup()
                         .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-                            .add(lblConversion)
-                            .add(layout.createSequentialGroup()
-                                .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-                                    .add(lblByteToReal)
-                                    .add(lblRealToByte)
-                                    .add(lblUnit))
-                                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
-                                .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-                                    .add(byteToReal)
-                                    .add(realToByte)
-                                    .add(unit)))
+                            .add(jPanel1, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
                             .add(layout.createSequentialGroup()
                                 .add(lblTable)
                                 .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
-                                .add(tableName, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 213, Short.MAX_VALUE)))
-                        .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
+                                .add(tableName, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 161, Short.MAX_VALUE)))
                         .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-                            .add(lblStorage)
                             .add(layout.createSequentialGroup()
-                                .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-                                    .add(lblStorageAddress)
-                                    .add(lblStorageSize)
-                                    .add(lblEndian))
                                 .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
-                                .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-                                    .add(endian)
-                                    .add(storageSize)
-                                    .add(storageAddress)))
+                                .add(jPanel2, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                             .add(layout.createSequentialGroup()
+                                .add(29, 29, 29)
                                 .add(lblCategory)
                                 .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
-                                .add(category)))
-                        .add(20, 20, 20)))
+                                .add(category)))))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
             .add(layout.createSequentialGroup()
                 .addContainerGap()
-                .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-                    .add(layout.createSequentialGroup()
-                        .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.BASELINE)
-                            .add(lblTable)
-                            .add(tableName))
-                        .add(20, 20, 20)
-                        .add(lblConversion)
-                        .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
-                        .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.TRAILING)
-                            .add(layout.createSequentialGroup()
-                                .add(lblUnit)
-                                .add(40, 40, 40))
-                            .add(layout.createSequentialGroup()
-                                .add(lblByteToReal)
-                                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
-                                .add(lblRealToByte))
-                            .add(layout.createSequentialGroup()
-                                .add(unit)
-                                .add(40, 40, 40))
-                            .add(layout.createSequentialGroup()
-                                .add(byteToReal)
-                                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
-                                .add(realToByte)))
-                        .add(26, 26, 26)
-                        .add(lblDescription))
-                    .add(layout.createSequentialGroup()
-                        .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.BASELINE)
-                            .add(lblCategory)
-                            .add(category))
-                        .add(20, 20, 20)
-                        .add(lblStorage)
-                        .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
-                        .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.BASELINE)
-                            .add(lblStorageSize)
-                            .add(storageSize))
-                        .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
-                        .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.BASELINE)
-                            .add(lblStorageAddress)
-                            .add(storageAddress))
-                        .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
-                        .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.BASELINE)
-                            .add(lblEndian)
-                            .add(endian))))
+                .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.BASELINE)
+                    .add(lblTable)
+                    .add(tableName)
+                    .add(lblCategory)
+                    .add(category))
                 .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
-                .add(description, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 60, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
+                .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING, false)
+                    .add(jPanel2, 0, 103, Short.MAX_VALUE)
+                    .add(jPanel1, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
+                .add(jPanel3, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
@@ -210,15 +254,16 @@ public class TablePropertyPanel extends javax.swing.JPanel {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel byteToReal;
     private javax.swing.JLabel category;
-    private javax.swing.JLabel description;
+    private javax.swing.JTextArea description;
     private javax.swing.JLabel endian;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JPanel jPanel3;
+    private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JLabel lblByteToReal;
     private javax.swing.JLabel lblCategory;
-    private javax.swing.JLabel lblConversion;
-    private javax.swing.JLabel lblDescription;
     private javax.swing.JLabel lblEndian;
     private javax.swing.JLabel lblRealToByte;
-    private javax.swing.JLabel lblStorage;
     private javax.swing.JLabel lblStorageAddress;
     private javax.swing.JLabel lblStorageSize;
     private javax.swing.JLabel lblTable;
