914aa796304acf34d649a726d2b08e7427f0be60
Jared Gould <jared.gould@gmail.com>
2006-07-03 13:40:18 +0000

    Started ECU definition editor
    git-svn-id: http://svn.3splooges.com/romraider-arch/trunk@72 d2e2e1cd-ba16-0410-be16-b7c4453c7c2d

---
+++ src/enginuity/definitions/DefinitionEditorMenuBar.java
@@ -0,0 +1,10 @@
+package enginuity.definitions;
+
+import javax.swing.JMenuBar;
+
+public class DefinitionEditorMenuBar extends JMenuBar {
+    
+    public DefinitionEditorMenuBar() {
+    }
+    
+}
