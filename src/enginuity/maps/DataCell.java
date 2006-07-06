b3cc1ebc225b92a668167d94901bcc46419051ff
drees <drees@romraider.forum>
2006-07-06 08:36:04 +0000

    Revert "fix" to setBinData.
    git-svn-id: http://svn.3splooges.com/romraider-arch/trunk@97 d2e2e1cd-ba16-0410-be16-b7c4453c7c2d

--- src/enginuity/maps/DataCell.java
+++ src/enginuity/maps/DataCell.java
@@ -85,6 +85,7 @@ public class DataCell extends JLabel implements MouseListener, Serializable {
     }
     
     public void setBinValue(double binValue) {
+		this.binValue = binValue;
 		// make sure it's in range
 		if (table.getStorageType() != Table.STORAGE_TYPE_FLOAT) {
 			if (binValue < 0) {
@@ -92,10 +93,8 @@ public class DataCell extends JLabel implements MouseListener, Serializable {
 			} else if (binValue > Math.pow(256, table.getStorageType()) - 1) {
 				this.setBinValue((int)(Math.pow(256, table.getStorageType()) - 1));
 			}
-			binValue = Math.round(binValue);
 		}
 
-		this.binValue = binValue;
 		this.updateDisplayValue();
 	}
     
