fca2b4919cd7b790afc87d46ab7fe322776bdfd0
kascade <kascade@romraider.forum>
2006-07-22 05:52:51 +0000

    just removed an unused constant
    git-svn-id: http://svn.3splooges.com/romraider-arch/trunk@169 d2e2e1cd-ba16-0410-be16-b7c4453c7c2d

--- src/enginuity/util/ColorScaler.java
+++ src/enginuity/util/ColorScaler.java
@@ -1,36 +1,36 @@
 package enginuity.util;
 
 import enginuity.Settings;
+
 import java.awt.*;
 
 public final class ColorScaler {
-    private static final int MAX_COLOR_INTENSITY = 255;
 
     private ColorScaler() {
     }
 
     public static Color getScaledColor(double scale, Settings settings) {
-        
+
         Color minColor = settings.getMinColor();
         Color maxColor = settings.getMaxColor();
-        
+
         float[] minColorHSB = new float[3];
         float[] maxColorHSB = new float[3];
-        
+
         Color.RGBtoHSB(minColor.getRed(),
                        minColor.getGreen(),
                        minColor.getBlue(),
                        minColorHSB);
-        
+
         Color.RGBtoHSB(maxColor.getRed(),
                        maxColor.getGreen(),
                        maxColor.getBlue(),
                        maxColorHSB);
-        
+
         float h = minColorHSB[0] + (maxColorHSB[0] - minColorHSB[0]) * (float)scale;
         float s = minColorHSB[1] + (maxColorHSB[1] - minColorHSB[1]) * (float)scale;
         float b = minColorHSB[2] + (maxColorHSB[2] - minColorHSB[2]) * (float)scale;
-        
+
         return Color.getHSBColor(h, s, b);
 
     }
