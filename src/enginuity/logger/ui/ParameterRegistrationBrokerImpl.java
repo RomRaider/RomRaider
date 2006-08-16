9f338d1dbd6455ab52b8d567d5cabd13299ba1f4
kascade <kascade@romraider.forum>
2006-08-16 13:25:14 +0000

    charts timescale set to seconds, max item count set to 2000
    git-svn-id: http://svn.3splooges.com/romraider-arch/trunk@244 d2e2e1cd-ba16-0410-be16-b7c4453c7c2d

--- src/enginuity/logger/ui/ParameterRegistrationBrokerImpl.java
+++ src/enginuity/logger/ui/ParameterRegistrationBrokerImpl.java
@@ -44,8 +44,10 @@ public final class ParameterRegistrationBrokerImpl implements ParameterRegistrat
 
             // add to charts
             final XYSeries series = new XYSeries(ecuParam.getName());
+            //TODO: Make chart max item count configurable via settings
+            series.setMaximumItemCount(2000);
             final XYDataset xyDataset = new XYSeriesCollection(series);
-            final JFreeChart chart = ChartFactory.createXYLineChart(ecuParam.getName(), "Time (ms)", ecuParam.getName() + " (" + ecuParam.getConvertor().getUnits() + ")",
+            final JFreeChart chart = ChartFactory.createXYLineChart(ecuParam.getName(), "Time (sec)", ecuParam.getName() + " (" + ecuParam.getConvertor().getUnits() + ")",
                     xyDataset, VERTICAL, false, true, false);
             ChartPanel chartPanel = new ChartPanel(chart, false, true, true, true, true);
             graphPanel.add(chartPanel);
@@ -63,7 +65,7 @@ public final class ParameterRegistrationBrokerImpl implements ParameterRegistrat
                     dataTableModel.updateParam(ecuParam, value);
 
                     // update graph
-                    series.add((System.currentTimeMillis() - loggerStartTime), ecuParam.getConvertor().convert(value));
+                    series.add((System.currentTimeMillis() - loggerStartTime) / 1000, ecuParam.getConvertor().convert(value));
 
                     // update dashboard
 
