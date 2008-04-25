83f11b50868bfae18aebd883769b6a16d4e93975
lizzardo <lizzardo@romraider.forum>
2008-04-25 16:20:36 +0000

    First pass at removing references to Enginuity.  Update copyright notices in those files that have one: - Change Enginuity/Enginuity.org to RomRaider/RomRaider.com - Update copyright date No code changes at this time.
    git-svn-id: https://svn2.assembla.com/svn/romraider/trunk@52 38686702-15cf-42e4-a595-3071df8bf5ea

--- src/enginuity/logger/ecu/ui/handler/livedata/LiveDataRow.java
+++ src/enginuity/logger/ecu/ui/handler/livedata/LiveDataRow.java
@@ -1,7 +1,7 @@
 /*
  *
- * Enginuity Open-Source Tuning, Logging and Reflashing
- * Copyright (C) 2006 Enginuity.org
+ * RomRaider Open-Source Tuning, Logging and Reflashing
+ * Copyright (C) 2006-2008 RomRaider.com
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
--- src/enginuity/newmaps/definition/RomDefinitionHandler.java
+++ src/enginuity/newmaps/definition/RomDefinitionHandler.java
@@ -1,6 +1,6 @@
 /*
  *
- * Copyright (C) 2006 Enginuity.org
+ * Copyright (C) 2006-2008 RomRaider.com
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
