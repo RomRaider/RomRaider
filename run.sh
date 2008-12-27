ff94ddec578bab96b3f06d5144c7330d4c607eba
kascade <kascade@romraider.forum>
2008-12-27 07:01:20 +0000

    update run.sh
    git-svn-id: https://svn2.assembla.com/svn/romraider/trunk@197 38686702-15cf-42e4-a595-3071df8bf5ea

--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-java -Djava.library.path=lib/linux -Dawt.useSystemAAFontSettings=lcd -Dswing.aatext=true -Dsun.java2d.d3d=false -Xms32M -Xmx384M -XX:-UseParallelGC -XX:CompileThreshold=10000 -jar RomRaider.jar >> romraider_sout.log 2>&1
+if [ -z "$JAVA_HOME" ]; then
+    echo JAVA_HOME not set.
+    exit 1
+fi
+
+$JAVA_HOME/bin/java -Djava.library.path=lib/linux -Dawt.useSystemAAFontSettings=lcd -Dswing.aatext=true -Dsun.java2d.d3d=false -Xms32M -Xmx384M -XX:-UseParallelGC -XX:CompileThreshold=10000 -jar RomRaider.jar >> romraider_sout.log 2>&1
 
 exit 0
