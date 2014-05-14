60aad987b1083ff97ece4f0930214735aab4a317
Dale Schultz <dschultzca@yahoo.ca>
2014-05-14 19:18:19 -0400

    Add additional possible correction detail for user to check.

--- src/main/java/com/romraider/logger/ecu/ui/swing/menubar/action/GlobalAdjustmentAction.java
+++ src/main/java/com/romraider/logger/ecu/ui/swing/menubar/action/GlobalAdjustmentAction.java
@@ -1,6 +1,6 @@
 /*
  * RomRaider Open-Source Tuning, Logging and Reflashing
- * Copyright (C) 2006-2013 RomRaider.com
+ * Copyright (C) 2006-2014 RomRaider.com
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -35,7 +35,7 @@ import com.romraider.swing.menubar.action.AbstractAction;
 import com.romraider.util.SettingsManager;
 
 public final class GlobalAdjustmentAction extends AbstractAction {
-    Settings settings = SettingsManager.getSettings();
+    final Settings settings = SettingsManager.getSettings();
 
     public GlobalAdjustmentAction(EcuLogger logger) {
         super(logger);
@@ -75,14 +75,14 @@ public final class GlobalAdjustmentAction extends AbstractAction {
         else if (result == 0) {
             showMessageDialog(logger,
                     "Error performing ECU Global Timing or Idle RPM adjustment.\n" +
-                            "Check the following:\n" +
-                            "* Logger has successfully conencted to the ECU\n" +
-                            "* Correct COM port is selected (if not Openport 2)\n" +
-                            "* CAN bus logging mode is disabled\n" +
-                            "* Cable is connected properly\n* Ignition is ON\n" +
-                            "* Logger is stopped\n",
-                            "Adjust ECU Global Parameters",
-                            ERROR_MESSAGE);
+                    "Check the following:\n" +
+                    "* Logger has successfully conencted to the ECU\n" +
+                    "* Correct COM port is selected (if not Openport 2)\n" +
+                    "* CAN bus logging mode is disabled\n" +
+                    "* Cable is connected properly\n* Ignition is ON\n" +
+                    "* Logger definition XML file is up to date\n",
+                    "Adjust ECU Global Parameters",
+                    ERROR_MESSAGE);
         }
         if (result == -1) {
             showMessageDialog(logger,
