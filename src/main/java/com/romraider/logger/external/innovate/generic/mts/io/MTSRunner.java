84990d59bec0f5e7d604e982cfcdcf406d79c983
Dale Schultz <dschultzca@yahoo.ca>
2015-02-12 23:36:09 -0500

    Refactor to take advantage of Properties defined in the lm2_mts.plugin to controller event mode or polled mode data updates

--- src/main/java/com/romraider/logger/external/innovate/generic/mts/io/MTSRunner.java
+++ src/main/java/com/romraider/logger/external/innovate/generic/mts/io/MTSRunner.java
@@ -1,6 +1,6 @@
 /*
  * RomRaider Open-Source Tuning, Logging and Reflashing
- * Copyright (C) 2006-2014 RomRaider.com
+ * Copyright (C) 2006-2015 RomRaider.com
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -44,17 +44,23 @@ public final class MTSRunner implements MTSEvents, Stoppable {
     private final MTS mts;
     private boolean running;
     private boolean stop;
+    private boolean pollMode;
 
     /**
      * MTSRunner contains the work-horse methods to process the MTS stream
      * data and update the appropriate sensor result.  Once started this class
-     * reads data at a set interval from the MTS stream.
-     * Events processing appears to be problematic via COM4J and therefore not used.
+     * listens to NewData Events.
+     * If Event processing appears problematic set the pollmode option to true
+     * in the lm2_mts.plugin file to enable reading data from the MTS stream
+     * rather than waiting for NewData events.
      */
-    public MTSRunner(int mtsPort, Map<Integer, Lm2MtsDataItem> dataItems) {
+    public MTSRunner(int mtsPort, Map<Integer,
+            Lm2MtsDataItem> dataItems,
+            boolean pollMode) {
         MTSConnector connection = new MTSConnector(mtsPort);
         this.mts = connection.getMts();
         this.dataItems = dataItems;
+        this.pollMode = pollMode;
     }
 
     public void run() {
@@ -84,8 +90,8 @@ public final class MTSRunner implements MTSEvents, Stoppable {
             try {
                 if (mts.inputCount() > 0) {
                     while (!stop) {
-//                        readData();
-                        sleep(2000L);
+                        if (pollMode) readData();
+                        sleep(40L);
                     }
                 }
                 else {
@@ -105,7 +111,7 @@ public final class MTSRunner implements MTSEvents, Stoppable {
     public void connectionEvent(int result) {
         if (result == 0) {
             mts.startData();
-            LOGGER.debug("Innovate MTS connection success");
+            LOGGER.debug("Innovate MTS connection success, pollmode:" + pollMode);
         }
         else if (result == -1) {
             throw new IllegalStateException("No Innovate MTS Data detected");
@@ -121,8 +127,10 @@ public final class MTSRunner implements MTSEvents, Stoppable {
     }
 
     public void newData() {
-        LOGGER.debug("Innovate MTS newData event");
-        readData();
+        if(!pollMode) {
+            LOGGER.debug("Innovate MTS newData event");
+            readData();
+        }
     }
     
     public void readData() {
@@ -134,68 +142,74 @@ public final class MTSRunner implements MTSEvents, Stoppable {
         float min;
         float max;
         float multiplier;
-        for (int i = 0; i < mts.inputCount(); i++) {
-            // select the input
-            dataItem = dataItems.get(i);
-            mts.currentInput(i);
-            type = mts.inputType();
-            function = mts.inputFunction();
-            sample = mts.inputSample();
-
-            // 5V channel
-            // Determine the range between min and max,
-            // calculate what percentage of that our sample represents,
-            // shift back to match our offset from 0.0 for min
-            if (type == MTS_TYPE_VDC.getType()) {
-                if (function == MTS_FUNC_NOTLAMBDA.getFunction()) {
-                    min = dataItem.getMinValue();
-                    max = dataItem.getMaxValue();
-                    data = ((max - min) * ((float) sample / 1024f)) + min;
+        try {
+            for (int i = 0; i < mts.inputCount(); i++) {
+                // select the input
+                dataItem = dataItems.get(i);
+                mts.currentInput(i);
+                type = mts.inputType();
+                function = mts.inputFunction();
+                sample = mts.inputSample();
+    
+                // 5V channel
+                // Determine the range between min and max,
+                // calculate what percentage of that our sample represents,
+                // shift back to match our offset from 0.0 for min
+                if (type == MTS_TYPE_VDC.getType()) {
+                    if (function == MTS_FUNC_NOTLAMBDA.getFunction()) {
+                        min = dataItem.getMinValue();
+                        max = dataItem.getMaxValue();
+                        data = ((max - min) * ((float) sample / 1024f)) + min;
+                    }
+                    else {
+                        // this will report other functions, such as ERROR states
+                        // as a negative constant value
+                        data = (float)function * -1f;
+                    }
                 }
-                else {
-                    // this will report other functions, such as ERROR states
-                    // as a negative constant value
-                    data = (float)function * -1f;
+    
+                // AFR
+                // Take each sample step as .001 Lambda,
+                // add 0.5 (so our range is 0.5 to 1.523 for our 1024 steps),
+                // then multiply by the AFR multiplier
+                if (type == MTS_TYPE_AFR.getType()) {
+                    if (function == MTS_FUNC_LAMBDA.getFunction()) {
+                        multiplier = dataItem.getMultiplier();
+                        data = ((float) sample / 1000f + 0.5f) * multiplier;
+                    }
+                    else if (function == MTS_FUNC_O2.getFunction()) {
+                        data = ((float) sample / 10f);
+                    }
+                    else {
+                        // this will report other functions, such as ERROR states
+                        // as a negative constant value
+                        data = (float)function * -1f;
+                    }
                 }
+    
+                // LAMBDA
+                // Identical to AFR, except we do not multiply for AFR.
+                if (type == MTS_TYPE_LAMBDA.getType()) {
+                    if (function == MTS_FUNC_LAMBDA.getFunction()) {
+                        data = (float) sample / 1000f + 0.5f;
+                    }
+                    else {
+                        // this will report other functions, such as ERROR states
+                        // as a negative constant value
+                        data = (float)function * -1f;
+                    }
+                }
+                // set data for this sensor based on inputNumber
+                if (dataItem != null) dataItem.setData(data);
+                final String result = String.format(
+                        "Innovate MTS input: %d, type: %d, function: %d, sample: %d, result: %f",
+                        i, type, function, sample, data);
+                LOGGER.trace(result);
             }
-
-            // AFR
-            // Take each sample step as .001 Lambda,
-            // add 0.5 (so our range is 0.5 to 1.523 for our 1024 steps),
-            // then multiply by the AFR multiplier
-            if (type == MTS_TYPE_AFR.getType()) {
-                if (function == MTS_FUNC_LAMBDA.getFunction()) {
-                    multiplier = dataItem.getMultiplier();
-                    data = ((float) sample / 1000f + 0.5f) * multiplier;
-                }
-                else if (function == MTS_FUNC_O2.getFunction()) {
-                    data = ((float) sample / 10f);
-                }
-                else {
-                    // this will report other functions, such as ERROR states
-                    // as a negative constant value
-                    data = (float)function * -1f;
-                }
-            }
-
-            // LAMBDA
-            // Identical to AFR, except we do not multiply for AFR.
-            if (type == MTS_TYPE_LAMBDA.getType()) {
-                if (function == MTS_FUNC_LAMBDA.getFunction()) {
-                    data = (float) sample / 1000f + 0.5f;
-                }
-                else {
-                    // this will report other functions, such as ERROR states
-                    // as a negative constant value
-                    data = (float)function * -1f;
-                }
-            }
-            // set data for this sensor based on inputNumber
-            if (dataItem != null) dataItem.setData(data);
-            final String result = String.format(
-                    "Innovate MTS input: %d, type: %d, function: %d, sample: %d, result: %f",
-                    i, type, function, sample, data);
-            LOGGER.trace(result);
+        }
+        catch (Exception e) {
+            LOGGER.error(String.format(
+                    "Innovate MTS read data error: %s", e.toString()));
         }
     }
 }
