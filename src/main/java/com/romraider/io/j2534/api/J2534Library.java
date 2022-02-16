db57badcf296fcf923f94c1d3b3dce466599d15f
Dale Schultz <dschultzca@yahoo.ca>
2022-02-15 19:52:31 -0500

    Update J2534Library class description

--- src/main/java/com/romraider/io/j2534/api/J2534Library.java
+++ src/main/java/com/romraider/io/j2534/api/J2534Library.java
@@ -1,6 +1,6 @@
 /*
  * RomRaider Open-Source Tuning, Logging and Reflashing
- * Copyright (C) 2006-2012 RomRaider.com
+ * Copyright (C) 2006-2022 RomRaider.com
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -21,9 +21,10 @@ package com.romraider.io.j2534.api;
 
 /**
  * Each class instance holds the Vendor and Library details for a J2534
- * installed device.  These are discovered on the local computer from
- * keys and value settings in the Windows registry.
- * @see J2534DllLocator 
+ * installed device.  On Windows these are discovered on the local computer
+ * from keys and value settings in the Windows registry.
+ * On Linux, from a static list.
+ * @see J2534DllLocator
  */
 public class J2534Library {
     private String vendor;
@@ -32,7 +33,7 @@ public class J2534Library {
     /**
      * Create a new instance of a J2534 library detail.
      * @param vendor    - J2534 vendor string
-     * @param library    - J2534 library path 
+     * @param library    - J2534 library path
      */
     public J2534Library(String vendor, String library) {
         this.vendor = vendor;
